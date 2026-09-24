8c5f0168b68547e0eb025f020ba38e5b3efd8277
metacyber26-source <metacyber26@gmail.com>
2026-09-24 13:49:40 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const metadata = {
+  title: "Secure Real-time Chat",
+  description: "Aplikasi Chatting Aman dan Scalable",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body style={{ margin: 0, padding: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
