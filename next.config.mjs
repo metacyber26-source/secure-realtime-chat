444e8185c024503c94e9334d3feb90dda7de91e9
metacyber26-source <metacyber26@gmail.com>
2026-09-24 14:47:30 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['sanitize-html'],
+  experimental: {
+    serverExternalPackages: ['sanitize-html'],
+  },
   async headers() {
     return [
       {
